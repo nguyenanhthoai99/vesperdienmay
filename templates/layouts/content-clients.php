052bd8ea9cac77a7e6451cf7b71924f9aad6d0e9
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2024-07-05 00:37:48 +0700

    Update content-clients.php

--- templates/layouts/content-clients.php
+++ templates/layouts/content-clients.php
@@ -1,6 +1,7 @@
 <?php require_once(__DIR__ . '/../../config.php'); ?>
 <!-- Content -->
 <div class="content">
+
     <!-- Sản phẩm khuyến mãi -->
     <div class="main-km">
         <h1>SẢN PHẨM KHUYẾN MÃI</h1>
@@ -47,10 +48,80 @@
             </div>
         </div>
     </div>
-    <div>
 
+    <!-- Sản phảm tivi -->
+
+    <div class="main-tivi row">
+        <h1>Tivi</h1>
+        <div class="card col-3 sp-tivi">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <span><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i><i class="fa-solid fa-star" style="color: #FFD43B;"></i></span>
+                <div class="d-grid gap-2 col-6 mx-auto">
+                    <a href="#" class="btn btn-primary">Mua</a>
+                </div>
+            </div>
+        </div>
+        <div class="card col-3 sp-tivi">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-3 sp-tivi">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-3 sp-tivi">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-3 sp-tivi">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-3 sp-tivi">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-3 sp-tivi">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="card col-3 sp-tivi">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
     </div>
-</div>
 
 
-<script type="text/javascript" src="<?php echo _WEB_HOST_TEMPLATES; ?>/js/jquery.min.js"></script>
+
+    <script type="text/javascript" src="<?php echo _WEB_HOST_TEMPLATES; ?>/js/jquery.min.js"></script>
