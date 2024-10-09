23834be1bd9bc86a9c9a9df793a5541a8cdf0743
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2024-10-10 03:03:40 +0700

    content items

--- templates/layouts/content.php
+++ templates/layouts/content.php
@@ -1,6 +1,9 @@
 <div class="container-fulid">
+    <!-- menu dọc -->
     <div class="row" style="margin: 0px">
+  
         <div class="col-4 menu-column fixed-top">
+        <h2 class="text-center text-upcase">Danh mục</h2>
             <ul class="nav flex-column">
                 <li class="nav-item">
                     <a class="nav-link" href="#"><img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/icon/maygiat.jpg" class="icon-column"> Máy giặt</a>
@@ -29,26 +32,116 @@
             </ul>
         </div>
         <div class="col-4"></div>
-        <div class="col-8 main-content">
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
-            <h1>Content</h1>
+
+        <!-- Nội dung chính của trang -->
+        <div class="col-8 content">
+            <a href="" class="sidebar img-thumbnail"><img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/sidebar/panasonic.jpg" alt="" class="img-sidebar"></a>
+            <div class="main-content">
+                <h2 class="tilte-content text-uppercase">Máy giặt</h2>
+                <div class="row">
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                    <a href="" class="col-3 main-item card" title="test">
+                        <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/may-giat-samsung-inverter-9-kg-w.jpg" class="card-img-top img-item" alt="..." onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                        <div class="card-body">
+                            <p class="card-title ten-item">Tên máy giặt</p>
+                            <p class="card-text gia-item">100000<span class="dong-item">&#8363;</span></p>
+                            <p class="card-text giacu-item">100000<span class="dong-item">&#8363;</span></p> 
+                            <p class="card-text qua-item">Quà</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+<script>
+    function increaseSize(el) {
+        $(el).css("position", "relative");
+        $(el).css("top", -10);
+    }
+
+    function decreaseSize(el) {
+        $(el).css("position", "relative");
+        $(el).css("top", 0);
+    }
+</script>
