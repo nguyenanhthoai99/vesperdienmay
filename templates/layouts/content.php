1e549d6bc53e4683edafd1970e953c32b51b9737
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2024-10-13 14:41:25 +0700

    update

--- templates/layouts/content.php
+++ templates/layouts/content.php
@@ -1,4 +1,3 @@
-
 <div class="container-fulid">
     <!-- menu dọc -->
     <div class="row" style="margin: 0px">
@@ -37,40 +36,40 @@
         <div class="col-8 content">
             <a href="" class="sidebar img-thumbnail"><img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/sidebar/panasonic.jpg" alt="" class="img-sidebar"></a>
             <div class="main-content">
-                <!-- Máy giặt -->
-                    <h2 class="tilte-content text-uppercase">Máy giặt</h2>
-                    <div class="row">
-                        <?php
-                        $queryMaygiat = getRaw("SELECT * FROM sanpham WHERE id_lsp = 1 ORDER BY update_at DESC, create_at DESC  limit 8");
-                        if (!empty($queryMaygiat)) :
-                            foreach ($queryMaygiat as $item):
-                        ?>
-                                <a href="maygiat/view.php?sp=<?php echo $item['ten_sp'] ?>" class="col-3 main-item card" title="<?php echo $item['ten_sp']; ?>">
-                                    <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/<?php echo $item['hinhanh']; ?>" class="card-img-top img-item" onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
-                                    <div class="card-body">
-                                        <p class="card-title ten-item"><?php echo limitString($item['ten_sp']); ?></p>
-                                        <p class="card-text gia-item"><?php echo showCurrency($item['giahientai_sp']); ?></p>
-                                        <?php echo !empty($item['giagoc_sp']) ? '<p class="card-text giacu-item">' . showCurrency($item['giagoc_sp']) . '</p>' : null ?>
-                                        <span class="phantram-item"><?php echo !empty(percentage($item['giagoc_sp'], $item['giahientai_sp'])) ? percentage($item['giagoc_sp'], $item['giahientai_sp']) : null; ?></span>
-                                    </div>
-                                    <input type="hidden" name="id_sp" value="<?php echo $item['id_sp'] ?>">
-                                </a>
-                        <?php
-                            endforeach;
-                        endif;
-                        ?>
-                    </div>
-                </form>
 
                 <!-- Máy giặt -->
                 <h2 class="tilte-content text-uppercase">Máy giặt</h2>
+                <div class="row">
+                    <?php
+                    $queryMaygiat = getRaw("SELECT * FROM sanpham WHERE id_lsp = 1 ORDER BY update_at DESC, create_at DESC  limit 8");
+                    if (!empty($queryMaygiat)) :
+                        foreach ($queryMaygiat as $item):
+                    ?>
+                            <a href="maygiat/view.php?sp=<?php echo $item['ten_sp'] ?>" class="col-3 main-item card" title="<?php echo $item['ten_sp']; ?>">
+                                <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maygiat/<?php echo $item['hinhanh']; ?>" class="card-img-top img-item" onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                                <div class="card-body">
+                                    <p class="card-title ten-item"><?php echo limitString($item['ten_sp']); ?></p>
+                                    <p class="card-text gia-item"><?php echo showCurrency($item['giahientai_sp']); ?></p>
+                                    <?php echo !empty($item['giagoc_sp']) ? '<p class="card-text giacu-item">' . showCurrency($item['giagoc_sp']) . '</p>' : null ?>
+                                    <span class="phantram-item"><?php echo !empty(percentage($item['giagoc_sp'], $item['giahientai_sp'])) ? percentage($item['giagoc_sp'], $item['giahientai_sp']) : null; ?></span>
+                                </div>
+                                <input type="hidden" name="id_sp" value="<?php echo $item['id_sp'] ?>">
+                            </a>
+                    <?php
+                        endforeach;
+                    endif;
+                    ?>
+                </div>
+
+                <!-- Máy lọc nước -->
+                <h2 class="tilte-content text-uppercase">Máy lọc nước</h2>
                 <div class="row">
                     <?php
                     $queryMayLocNuoc = getRaw("SELECT * FROM sanpham WHERE id_lsp = 2 ORDER BY update_at DESC, create_at DESC  limit 8");
                     if (!empty($queryMayLocNuoc)) :
                         foreach ($queryMayLocNuoc as $item):
                     ?>
-                            <a href="" class="col-3 main-item card" title="<?php echo $item['ten_sp']; ?>">
+                            <a href="maylocnuoc/view.php?sp=<?php echo $item['ten_sp'] ?>" class="col-3 main-item card" title="<?php echo $item['ten_sp']; ?>">
                                 <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maylocnuoc/<?php echo $item['hinhanh']; ?>" class="card-img-top img-item" onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
                                 <div class="card-body">
                                     <p class="card-title ten-item"><?php echo limitString($item['ten_sp']); ?></p>
@@ -84,8 +83,54 @@
                     endif;
                     ?>
                 </div>
-            </div>
 
+                <!-- Tủ lạnh -->
+                <h2 class="tilte-content text-uppercase">Tủ lạnh</h2>
+                <div class="row">
+                    <?php
+                    $queryTuLanh = getRaw("SELECT * FROM sanpham WHERE id_lsp = 6 ORDER BY update_at DESC, create_at DESC  limit 8");
+                    if (!empty($queryTuLanh)) :
+                        foreach ($queryTuLanh as $item):
+                    ?>
+                            <a href="tulanh/view.php?sp=<?php echo $item['ten_sp'] ?>" class="col-3 main-item card" title="<?php echo $item['ten_sp']; ?>">
+                                <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/tulanh/<?php echo $item['hinhanh']; ?>" class="card-img-top img-item" onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                                <div class="card-body">
+                                    <p class="card-title ten-item"><?php echo limitString($item['ten_sp']); ?></p>
+                                    <p class="card-text gia-item"><?php echo showCurrency($item['giahientai_sp']); ?></p>
+                                    <?php echo !empty($item['giagoc_sp']) ? '<p class="card-text giacu-item">' . showCurrency($item['giagoc_sp']) . '</p>' : null ?>
+                                    <span class="phantram-item"><?php echo !empty(percentage($item['giagoc_sp'], $item['giahientai_sp'])) ? percentage($item['giagoc_sp'], $item['giahientai_sp']) : null; ?></span>
+                                </div>
+                            </a>
+                    <?php
+                        endforeach;
+                    endif;
+                    ?>
+                </div>
+
+                <!-- máy lạnh -->
+                <h2 class="tilte-content text-uppercase">Máy lạnh</h2>
+                <div class="row">
+                    <?php
+                    $queryMayLanh = getRaw("SELECT * FROM sanpham WHERE id_lsp = 7 ORDER BY update_at DESC, create_at DESC  limit 8");
+                    if (!empty($queryMayLanh)) :
+                        foreach ($queryMayLanh as $item):
+                    ?>
+                            <a href="maylanh/view.php?sp=<?php echo $item['ten_sp'] ?>" id="main-maylanh" class="col-3 main-item card" title="<?php echo $item['ten_sp']; ?>">
+                                <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/maylanh/<?php echo $item['hinhanh']; ?>" class="card-img-top img-item img-maylanh" onmouseenter="increaseSize(this)" onmouseout="decreaseSize(this)">
+                                <div class="card-body">
+                                    <p class="card-title ten-item"><?php echo limitString($item['ten_sp']); ?></p>
+                                    <p class="card-text gia-item"><?php echo showCurrency($item['giahientai_sp']); ?></p>
+                                    <?php echo !empty($item['giagoc_sp']) ? '<p class="card-text giacu-item">' . showCurrency($item['giagoc_sp']) . '</p>' : null ?>
+                                    <span class="phantram-item"><?php echo !empty(percentage($item['giagoc_sp'], $item['giahientai_sp'])) ? percentage($item['giagoc_sp'], $item['giahientai_sp']) : null; ?></span>
+                                </div>
+                            </a>
+                    <?php
+                        endforeach;
+                    endif;
+                    ?>
+                </div>
+            </div>
         </div>
+
     </div>
 </div>
--- templates/layouts/header.php
+++ templates/layouts/header.php
@@ -17,22 +17,19 @@
 <div class="container">
     <nav id="header" class="navbar navbar-expand-lg fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand ms-5" href="<?php echo _WEB_HOST;?>/index.php"><img src="<?php echo _WEB_HOST_TEMPLATES ?>/images/icon/icon.jpg" class="hinh-icon" />Vesper Nguyễn</a>
+            <a class="navbar-brand ms-5" href="<?php echo _WEB_HOST; ?>/index.php"><img src="<?php echo _WEB_HOST_TEMPLATES ?>/images/icon/icon.jpg" class="hinh-icon" />Vesper Nguyễn</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse menu-header" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-outline-primary" aria-current="page" href="#">Giới thiệu</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-outline-primary" aria-current="page" href="#">Liên hệ</a>
-                    </li>
                     <form class="d-flex" role="search" method="get">
                         <input class="form-control me-2" type="search" placeholder="Nhập sản phẩm bạn muốn tìm kiếm" aria-label="search" style="width : 500px">
                         <button class="btn btn-search" type="submit">Tìm kiếm</button>
                     </form>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-outline-primary" aria-current="page" href="<?php echo _WEB_HOST ?>/gioithieu.php">Giới thiệu</a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link btn btn-outline-primary" href="<?php echo _WEB_HOST ?>/auth/login.php"><i class="fa-solid fa-user"></i> Đăng nhập</a>
                     </li>
