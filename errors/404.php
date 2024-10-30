c6f8876c0f890944fafd1f59a537116c22fcc7cb
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2024-10-31 00:51:39 +0700

    Update 404.php

--- errors/404.php
+++ errors/404.php
@@ -1,7 +1,1 @@
-<?php
-// Kiểm tra quyền truy cập
-if (!defined('_CODE')) {
-    die('<h1>Trang không tồi tại hoặc bạn không có quyền truy cập trang này</h1>');
-}
-?>
 <h1>Trang không tồi tại hoặc bạn không có quyền truy cập trang này</h1>
