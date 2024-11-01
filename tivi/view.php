613b1568eb3cd9226b551e164c51eaf9e56f8c02
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2024-11-02 01:24:02 +0700

    Update view.php

--- tivi/view.php
+++ tivi/view.php
@@ -18,130 +18,135 @@ $giaLon = $query['giahientai_sp'] + 5000000;
 <div class="container-fluid" style="margin-top: 100px; background: white">
     <!-- Thông tin sản phẩm -->
     <div class="row" style="padding:15px 5px">
+
         <div class="col-5">
-            <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/tivi/<?php echo $query['hinhanh']; ?>"class="card-img-top">
+            <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/tivi/<?php echo $query['hinhanh']; ?>" class="card-img-top">
         </div>
         <div class="col-7">
-            <table class="table table-borderless" style="margin-bottom:0px;">
-                <h3 class="tieudieu-spct text-center">Thông tin chi tiết</h3>
-                <tbody>
-                    <div class="row">
-                        <tr>
-                            <div class="col-3">
+            <form action="<?php _WEB_HOST; ?>/vesperdienmay/cart/dathang.php" method="post">
+                <table class="table table-borderless" style="margin-bottom:0px;">
+                    <h3 class="tieudieu-spct text-center">Thông tin chi tiết</h3>
+                    <tbody>
+                        <div class="row">
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Loại Tivi:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['loai_tv'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Kích cỡ màn hình:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['kichco_tv'] ?> inch</td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Độ phân giải:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['dophangiai_tv'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Loại màn hình:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['loaimanhinh_tv'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Hệ điều hành:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['hedieuhanh_tv'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Chất liệu chân đế:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['chatlieuchande_tv'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Chất liệu viền tivi:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['chatlieuvien_tv'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Sản xuất:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['ten_nsx'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Năm:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['nam'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
                                 <td>
-                                Loại Tivi:
+                                    <p class="card-text gia-item"><?php echo showCurrency($query['giahientai_sp']); ?></p>
+                                    <?php echo !empty($query['giagoc_sp']) ? '<p class="card-text giacu-item">' . showCurrency($query['giagoc_sp']) . '</p>' : null ?>
+                                    <span class="phantram-goiy"><?php echo !empty(percentage($query['giagoc_sp'], $query['giahientai_sp'])) ? percentage($query['giagoc_sp'], $query['giahientai_sp']) : null; ?></span>
                                 </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['loai_tv'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                Kích cỡ màn hình:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['kichco_tv'] ?> inch</td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                Độ phân giải:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['dophangiai_tv'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                Loại màn hình:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['loaimanhinh_tv'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                Hệ điều hành:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['hedieuhanh_tv'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                Chất liệu chân đế:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['chatlieuchande_tv'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                Chất liệu viền tivi:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['chatlieuvien_tv'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                    Sản xuất:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['ten_nsx'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                    Năm:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['nam'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p class="card-text gia-item"><?php echo showCurrency($query['giahientai_sp']); ?></p>
-                                <?php echo !empty($query['giagoc_sp']) ? '<p class="card-text giacu-item">' . showCurrency($query['giagoc_sp']) . '</p>' : null ?>
-                                <span class="phantram-goiy"><?php echo !empty(percentage($query['giagoc_sp'], $query['giahientai_sp'])) ? percentage($query['giagoc_sp'], $query['giahientai_sp']) : null; ?></span>
-                            </td>
-                        </tr>
+                            </tr>
+                        </div>
+                    </tbody>
+                </table>
+                <div class="row">
+                    <label for="soluong">Số lượng đặt mua:</label>
+                    <div class="col-3 main-btnaddnumber">
+                        <button type="button" id="btn-Tru" name="btn-Tru" style="width:50px;" onclick="tru()">
+                            <i class="fa fa-minus"></i>
+                        </button>
+                        <input type="number" min="1" max="9" class="form-control" id="soluongmua" name="soluongmua" style="width:100px; text-align:center;" value="1">
+                        <button type="button" id="btn-Cong" name="btn-Cong" style="width:50px;" onclick="cong()">
+                            <i class="fa fa-plus"></i>
+                        </button>
                     </div>
-                </tbody>
-            </table>
-            <div class="row">
-                <label for="soluong">Số lượng đặt mua:</label>
-                <div class="col-3 main-btnaddnumber">
-                    <button type="button" id="btn-Tru" name="btn-Tru" style="width:50px;" onclick="tru()">
-                        <i class="fa fa-minus"></i>
-                    </button>
-                    <input type="number" min="1" max="9" class="form-control" id="soluongmua" name="soluongmua" style="width:100px; text-align:center;" value="1">
-                    <button type="button" id="btn-Cong" name="btn-Cong" style="width:50px;" onclick="cong()">
-                        <i class="fa fa-plus"></i>
-                    </button>
+                    <input type="hidden" name="id_sp" value="<?php echo $id; ?>" />
+                    <div class="col-9">
+                        <button type="submit" style="width:100%" class="btn btn-primary btn-mua">Mua ngay</button>
+                    </div>
                 </div>
-                <div class="col-9">
-                    <button type="button" style="width:100%" class="btn btn-primary btn-mua"><a href="<?php echo _WEB_HOST; ?>/cart">Mua ngay</a></button>
-                </div>
-            </div>
+            </form>
         </div>
+
 
         <!-- Sản phẩm gợi ý -->
         <div class="main-goiy" style="margin: 100px 0px;">
