bdfbc764c3318b159ad9c0031ee8321dc08d2369
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2024-11-02 01:45:09 +0700

    Update view.php

--- tulanh/view.php
+++ tulanh/view.php
@@ -21,115 +21,118 @@ $giaLon = $query['giahientai_sp'] + 5000000;
             <img src="<?php echo _WEB_HOST_TEMPLATES; ?>/images/tulanh/<?php echo $query['hinhanh']; ?>" class="img-sanphamchitiet">
         </div>
         <div class="col-7">
-            <table class="table table-borderless">
-                <h3 class="tieudieu-spct text-center">Thông tin chi tiết</h3>
-                <tbody>
-                    <div class="row">
-                        <tr>
-                            <div class="col-3">
+            <form action="<?php _WEB_HOST; ?>/vesperdienmay/cart/dathang.php" method="post">
+                <table class="table table-borderless">
+                    <h3 class="tieudieu-spct text-center">Thông tin chi tiết</h3>
+                    <tbody>
+                        <div class="row">
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Kiểu tủ:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['kieu_tl'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Dung tích tổng:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['dungtich_tl'] ?> Lít</td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Dung tích ngăn đá:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['dungtichnganda_tl'] ?> Lít</td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Dung tích ngăn lạnh:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['dungtichnganlanh_tl'] ?> Lít</td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Chất lượng cửa tủ lạnh:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['chatlieucua_tl'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Chất liệu ống dẫn gas, dàn lạnh:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['chatlieuonggas_tl'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Sản xuất:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['ten_nsx'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
+                                <div class="col-3">
+                                    <td>
+                                        Năm:
+                                    </td>
+                                </div>
+                                <div class="col-9">
+                                    <td><?php echo $query['nam'] ?></td>
+                                </div>
+                            </tr>
+                            <tr>
                                 <td>
-                                    Kiểu tủ:
+                                    <p class="card-text gia-item"><?php echo showCurrency($query['giahientai_sp']); ?></p>
+                                    <?php echo !empty($query['giagoc_sp']) ? '<p class="card-text giacu-item">' . showCurrency($query['giagoc_sp']) . '</p>' : null ?>
+                                    <span class="phantram-goiy"><?php echo !empty(percentage($query['giagoc_sp'], $query['giahientai_sp'])) ? percentage($query['giagoc_sp'], $query['giahientai_sp']) : null; ?></span>
                                 </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['kieu_tl'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                    Dung tích tổng:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['dungtich_tl'] ?> Lít</td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                    Dung tích ngăn đá:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['dungtichnganda_tl'] ?> Lít</td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                    Dung tích ngăn lạnh:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['dungtichnganlanh_tl'] ?> Lít</td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                    Chất lượng cửa tủ lạnh:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['chatlieucua_tl'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                    Chất liệu ống dẫn gas, dàn lạnh:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['chatlieuonggas_tl'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                    Sản xuất:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['ten_nsx'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <div class="col-3">
-                                <td>
-                                    Năm:
-                                </td>
-                            </div>
-                            <div class="col-9">
-                                <td><?php echo $query['nam'] ?></td>
-                            </div>
-                        </tr>
-                        <tr>
-                            <td>
-                                <p class="card-text gia-item"><?php echo showCurrency($query['giahientai_sp']); ?></p>
-                                <?php echo !empty($query['giagoc_sp']) ? '<p class="card-text giacu-item">' . showCurrency($query['giagoc_sp']) . '</p>' : null ?>
-                                <span class="phantram-goiy"><?php echo !empty(percentage($query['giagoc_sp'], $query['giahientai_sp'])) ? percentage($query['giagoc_sp'], $query['giahientai_sp']) : null; ?></span>
-                            </td>
-                        </tr>
+                            </tr>
+                        </div>
+                    </tbody>
+                </table>
+                <div class="row">
+                    <label for="soluong">Số lượng đặt mua:</label>
+                    <div class="col-3 main-btnaddnumber">
+                        <button type="button" id="btn-Tru" name="btn-Tru" style="width:50px;" onclick="tru()">
+                            <i class="fa fa-minus"></i>
+                        </button>
+                        <input type="number" min="1" max="9" class="form-control" id="soluongmua" name="soluongmua" style="width:100px; text-align:center;" value="1">
+                        <button type="button" id="btn-Cong" name="btn-Cong" style="width:50px;" onclick="cong()">
+                            <i class="fa fa-plus"></i>
+                        </button>
                     </div>
-                </tbody>
-            </table>
-            <div class="row">
-                <label for="soluong">Số lượng đặt mua:</label>
-                <div class="col-3 main-btnaddnumber">
-                    <button type="button" id="btn-Tru" name="btn-Tru" style="width:50px;" onclick="tru()">
-                        <i class="fa fa-minus"></i>
-                    </button>
-                    <input type="number" min="1" max="9" class="form-control" id="soluongmua" name="soluongmua" style="width:100px; text-align:center;" value="1">
-                    <button type="button" id="btn-Cong" name="btn-Cong" style="width:50px;" onclick="cong()">
-                        <i class="fa fa-plus"></i>
-                    </button>
+                    <input type="hidden" name="id_sp" value="<?php echo $id; ?>" />
+                    <div class="col-9">
+                        <button type="submit" style="width:100%" class="btn btn-primary btn-mua">Mua ngay</button>
+                    </div>
                 </div>
-                <div class="col-9">
-                    <button type="button" style="width:100%" class="btn btn-primary btn-mua"><a href="<?php echo _WEB_HOST; ?>/cart">Mua ngay</a></button>
-                </div>
-            </div>
+            </form>
         </div>
     </div>
 
