5950d663a6599064a7521b996e44c43f2c35a69e
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2024-11-03 17:06:25 +0700

    Update logout.php

--- auth/logout.php
+++ auth/logout.php
@@ -6,10 +6,12 @@ require_once(_WEB_PATH . '/includes/connect.php');
 require_once(_WEB_PATH . '/includes/database.php');
 require_once(_WEB_PATH . '/includes/session.php');
 
+setcookie("user_login", "", time() - (60 * 60 * 24 * 30 * 12), "/");
 if (isLogin()) {
     $token = getSession('tokenLogin');
     delete('tokenlogin', "token = '$token'");
     removeSession('tokenLogin');
-    removeSession('id_dangnhap');
+    removeSession('user_login');
     redirect(_WEB_HOST . '/auth/login.php');
 }
+redirect(_WEB_HOST . '/auth/login.php');
