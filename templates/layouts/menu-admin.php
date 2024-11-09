bcc45ce653fdac04a71e61e7a772af6c221489a9
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2024-11-10 01:32:32 +0700

    Create menu-admin.php

---
+++ templates/layouts/menu-admin.php
@@ -0,0 +1,28 @@
+<div class="col-4 menu-column">
+    <h2 class="text-center text-upcase">Danh mục</h2>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="<?php _WEB_HOST; ?>/vesperdienmay/admin/dashboard.php"> Dashboard</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php _WEB_HOST; ?>/vesperdienmay/users/">Quản lý tài khoản</a>
+        </li>
+        <li class="nav-item dropdown btn-group dropend">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Quản lý sản phẩm
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="<?php echo _WEB_HOST; ?>/maygiat/list.php">Máy giặt</a></li>
+                <li><a class="dropdown-item" href="<?php echo _WEB_HOST; ?>/maylocnuoc/list.php">Máy lọc nước</a></li>
+                <li><a class="dropdown-item" href="<?php echo _WEB_HOST; ?>/tulanh/list.php">Tủ lạnh</a></li>
+                <li><a class="dropdown-item" href="<?php echo _WEB_HOST; ?>/maygiat/list.php">Máy lạnh</a></li>
+                <li><a class="dropdown-item" href="<?php echo _WEB_HOST; ?>/tudong/list.php">Tủ đông</a></li>
+                <li><a class="dropdown-item" href="<?php echo _WEB_HOST; ?>/maynuocnong/list.php">Máy nước nóng</a></li>
+                <li><a class="dropdown-item" href="<?php echo _WEB_HOST; ?>/tivi/list.php">Tivi</a></li>
+            </ul>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="<?php _WEB_HOST; ?>/vesperdienmay/maylanh">Thống kê</a>
+        </li>
+    </ul>
+</div>
