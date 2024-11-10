2a44997eed202a5b238af093090a88bab47135a8
nguyenanhthoai99 <69306469+nguyenanhthoai99@users.noreply.github.com>
2024-11-11 02:26:37 +0700

    Update admin.js

---
+++ templates/js/admin.js
@@ -0,0 +1,30 @@
+
+document.getElementById('sp_hinh').onchange = function(evt) {
+    var tgt = evt.target || window.event.srcElement,
+        files = tgt.files;
+
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function() {
+            document.getElementById("hinhDemo").src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+    } else {
+
+    }
+}
+
+document.getElementById('sp_hinhchitiet').onchange = function(evt) {
+    var tgt = evt.target || window.event.srcElement,
+        files = tgt.files;
+
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function() {
+            document.getElementById("hinhDemoChiTiet").src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+    } else {
+
+    }
+}
